2996c3526efdf1bb9d0a6c73848f598feb129efe
JGSNETO <jgsnto.sd@gmail.com>
2024-08-16 22:41:02 +0200

    C4M1L1 & C4M1L2

---
+++ C4/M1/C4M1L1_Overview.cpp
@@ -0,0 +1,8 @@
+// C++ 11
+/*
+
+- More standard template library.
+- New features of C++ 11, such as move semantics, and lambda expressions.
+- AI techniques used in implementing the game of Hex.
+
+*/
