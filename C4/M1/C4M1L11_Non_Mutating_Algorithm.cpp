d62b9c206f9bb12588517aa4e078cd705456929a
JGSNETO <jgsnto.sd@gmail.com>
2024-08-20 22:07:33 +0200

    C4M1L11: Non mutating algorithm

---
+++ C4/M1/C4M1L11_Non_Mutating_Algorithm.cpp
@@ -0,0 +1,6 @@
+/*
+# Non-mutating sequence algorithm
+- Do not modify contents of the containers they work on.
+- Typical operation is searching container for particular element and returning its position.
+
+ */
