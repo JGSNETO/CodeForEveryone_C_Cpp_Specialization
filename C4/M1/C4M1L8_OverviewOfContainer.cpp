300da955525109d4a124ce42b0991eb052dcaa98
JGSNETO <jgsnto.sd@gmail.com>
2024-08-20 21:27:48 +0200

    C1M4L8: Overview of containers

---
+++ C4/M1/C4M1L8_OverviewOfContainer.cpp
@@ -0,0 +1,30 @@
+/*
+So, initially, C++, the initial innovation was turning C into an object-oriented language by creating the concept class and data hiding within the C language. 
+That was originally called C with classes and then C++ in 1985. 
+
+- Standard template library accepted in july 1994 into C++ ANSI Standard. 
+- STL library provides containers, iterators and algorithms designed to:
+1. Work efficiently
+2. Work parametrically
+3. Work orthogonally. 
+
+# Two varieties of containers
+1. Sequence: ordered by sequence of elements
+2. Associative: Keys for looking up elements. (Deque, list, vector, set, multiset, map, multimap)
+
+# Typical Container interfaces
+- Constructors: Default constructors, copy contructors
+- Elements access
+- Element insertion
+- Element emplacement - new C++11
+- Element deletion
+- Destructor
+- Iterators
+
+# Overview of Containers
+- Common set of properties
+- Constructores and destructors
+- Elements access, insertion and deletion
+- Allocate and manage memory
+
+*/
