dc65b5612f11ffcafe2082030d696858b2d1b208
JGSNETO <jgsnto.sd@gmail.com>
2024-08-20 21:44:30 +0200

    C4M1L9: Associative containers

---
+++ C4/M1/C4M1L9_AssociativeContainers.cpp
@@ -0,0 +1,22 @@
+/*
+# Associative Containers
+- key based accessible elements: set, multiset, map, multimap
+- Ordering relation: Compare
+- Comparison object for the associative container
+
+# Unordered_map
+
+- C++11 introduces "hash" based lookup for map and set.
+- unordered_map: Uses hash lookup
+- unordered_set: Uses hash lookup
+
+- Advantage: Much of the time O(1) lookup
+
+# Map program
+
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <iostream>
+using namespace std;
+*/
