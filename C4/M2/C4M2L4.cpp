4c9962b054a29914a6bd1ffce19f2854b06f183c
JGSNETO <jgsnto.sd@gmail.com>
2024-08-28 23:13:19 +0200

    C2M4L4 & 5

---
+++ C4/M2/C4M2L4.cpp
@@ -0,0 +1,20 @@
+/*
+# The inheritance Mechanism
+
+- Means of deriving new class from new class from existing classes, called base classes.
+- Reuses existing code eliminating tedious, error prone task of developing new code.
+- Derived class developed from base by adding or altering code.
+- Hierarchy of related types created that share code & interface.
+
+# Single x Multiple
+
+- Single inheritance occurs when single base class.
+- Multiple inheritance occurs when more than one base class.
+
+# Taxonomyc Classification
+
+- In C++ terms, classes elephand and mouse derived from base class "mammal"
+- In OOP terms, elephant ISA mammal describes relationship.
+- If circus has elephants, then object circus might have members of type elephant.
+- Class circus HASA elephant describes subpart relationship.
+*/
---
+++ C4/M2/C4M2L5.cpp
@@ -0,0 +1,16 @@
+/*
+# Virtual Member Functions
+
+- Functions declared in base class and redefined in derived class.
+- Class hierarchy defined by public inheritance creates related set of user types, 
+all of whose objects may be pointed at by a base class pointer.
+- By Accesssing virtual function through this pointer, C++ selects appropriate function definition at run-time.
+
+
+# Pure polymorphism
+
+- Object being pointed at must carry around type information so distinction can be made dynamically
+- Each object "knows" how it is acted on.
+- Inheritance designed into SW to maximize reuse and allow natural modeling of problem domain. 
+
+*/
