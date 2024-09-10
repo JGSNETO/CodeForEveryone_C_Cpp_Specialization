738ba2addba1b725cff0b7b4663493b9d4d5732a
JGSNETO <jgsnto.sd@gmail.com>
2024-09-10 22:22:37 +0200

    C4M4L9: Design pattern

---
+++ C4/M4/C4M4L9.cpp
@@ -0,0 +1,33 @@
+/*
+# Design Patterns 
+
+- Design patterns are reusable solutions to common problems encountered in software design. They offer a standard approach to solving these problems
+in a way that is flexible, maintainable, and scalable. Design petterns are categorized into three main types: creational, structual ang behavioral pattern.
+
+## Creational Patterns
+
+- These patterns deal with object creation mechanisms, trying to create objects in a manner suitable to the situation.
+1. Singleton Pattern: Ensures a class has only one instance and provides a global point of access to that instance.
+2. Factory Pattern: Provies an interface for creating objects but lets subclasses alter the type of objects that will be created.
+
+## Structured Pattern
+
+- These patterns focus on the composition of classes and objects, typically dealing with relationships between them. 
+
+1. Adapter Pattern: Allows two incompatible interfaces to work together. It wraps an existing class with a new interface. 
+2. Decorator Pattern: Adds behavior to objects dynamically by wrapping them in a decorator object. 
+
+## Behavioral Patterns
+
+- These patterns are concerned with algorithms and the assignment of responsibilities between objects.
+
+1. Observer Pattern: Defines a one-to-many dependency between objects so that when one object changes state, all its dependents are notified and updated automatically.
+2. Strategiv Pattern: Defines a family of algorithms and lets the client choose one at runtime.
+
+## Benefits of using Design Patterns
+
+1. Reusability: Patterns provide reusable solutions to common problems, making your code more modular and easier to understand.
+2. Maintainability: By using a well-known patterns, your design becomes easier for other developers to understand and modify.
+3. Scalabitly: Design patterns help in building flexible and extensible software architectures.
+4. Testability: They often improve code testability by enforcing separation of concerns.
+*/
