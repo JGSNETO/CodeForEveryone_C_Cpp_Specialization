31a10dae26203129ccc8a6bcc83600f0f9d62fd3
JGSNETO <jgsnto.sd@gmail.com>
2024-09-12 22:25:53 +0200

    C4M4L10: Standard

---
+++ C4/M4/C4M4L10.cpp
@@ -0,0 +1,23 @@
+/*
+# CPP 11 Standard
+
+- Added critical libraries, such as:
+1. Tuple - pair and more.
+2. Array - Fixed length array container
+3. Forward_list - Single pointer list
+4. Unordered_map, unordered_set - Hashing used
+5. Thread - Uniform thread interface + other libraries
+6. Regex -  regular expressions
+7. Type_traits - type characteristics
+
+## C++ Standard Versions: Over time, the C++ standard has evolved, with new versions introducing new features, libraries, and improvements. Each version is typically named based on the year it was published. Below are some of the major C++ standards:
+
+- C++98 (ISO/IEC 14882:1998): The first standardized version of C++.
+- C++03 (ISO/IEC 14882:2003): A bug-fix release to C++98, with no major language features added.
+- C++11 (ISO/IEC 14882:2011): This version introduced significant new features such as smart pointers, lambdas, move semantics, nullptr, auto keyword, range-based for loops, and more.
+- C++14 (ISO/IEC 14882:2014): A minor update to C++11, introducing improvements like generic lambdas, relaxed constexpr, and other refinements.
+- C++17 (ISO/IEC 14882:2017): Added new language features like std::optional, std::variant, std::filesystem, and fold expressions for variadic templates.
+- C++20 (ISO/IEC 14882:2020): This version was a major update, introducing concepts, ranges, coroutines, modules, and more. It significantly expanded the capabilities of the language.
+- C++23: The upcoming standard that builds upon C++20, adding more features and improvements. It is expected to be finalized in 2023.
+
+*/
